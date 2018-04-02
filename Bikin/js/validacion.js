6f25d555024ad3e6ab3f1dfcc360322cb29a8efb
andres <andresbalestrini@gmail.com>
2018-04-02 13:32:11 -0300

    form correction and sending of email

--- Bikin/js/validacion.js
+++ Bikin/js/validacion.js
@@ -14,57 +14,56 @@ jQuery(document).ready(function () {
 
 var jForm = {
     valid: function (event) {
-        console.log("valid function");
-        if ($('[name=debut]:checked').length == 0){
+        if ($('[name=debut]:checked').length == 0) {
             alert("Debe responder la primera pregunta");
             $('[name=debut]:first').focus();
             event.preventDefault();
             return;
         }
 
-        if ($('[name=arcampeon]:checked').length == 0){
+        if ($('[name=arcampeon]:checked').length == 0) {
             alert("Debe responder la segunda pregunta");
             $('[name=arcampeon]:first').focus();
             event.preventDefault();
             return;
         }
 
-        if ($('[name=maxpresencias]:checked').length == 0){
+        if ($('[name=maxpresencias]:checked').length == 0) {
             alert("Debe responder la tercera pregunta");
             $('[name=maxpresencias]:first').focus();
             event.preventDefault();
             return;
         }
 
-        if ($('[name=maxgoles]:checked').length == 0){
+        if ($('[name=maxgoles]:checked').length == 0) {
             alert("Debe responder la cuarta pregunta");
             $('[name=maxgoles]:first').focus();
             event.preventDefault();
             return;
         }
 
-        if ($('[name=campeones86]:checked').length == 0){
+        if ($('[name=campeones86]:checked').length == 0) {
             alert("Debe responder la quinta pregunta");
             $('[name=campeones86]:first').focus();
             event.preventDefault();
             return;
         }
 
-        if ($('[name=dtargentina]:checked').length == 0){
+        if ($('[name=dtargentina]:checked').length == 0) {
             alert("Debe responder la sexta pregunta");
             $('[name=dtargentina]:first').focus();
             event.preventDefault();
             return;
         }
 
-        if ($('[name=goles2014]:checked').length == 0){
+        if ($('[name=goles2014]:checked').length == 0) {
             alert("Debe responder la septima pregunta");
             $('[name=goles2014]:first').focus();
             event.preventDefault();
             return;
         }
 
-        if ($('[name=rivalesarg]:checked').length == 0){
+        if ($('[name=rivalesarg]:checked').length == 0) {
             alert("Debe responder la octava pregunta");
             $('[name=rivalesarg]:first').focus();
             event.preventDefault();
@@ -115,7 +114,135 @@ var jForm = {
             return;
         }
 
+
+        // como esta todo completado comienzo a poner colores de correccion
+        this.correccion();
+    },
+
+    correccion: function () {
+        var debutMaradona = [];
+        $('[name=debut]:checked').each(function () {
+            debutMaradona.push($(this).val());
+        });
+        if (!debutMaradona.includes("ves82")) {
+            if ($('#correccionpreg1').html('') != '') { $('#correccionpreg1').html(''); }
+            $('#correccionpreg1').append("Incorrecto, la respuesta correcta es España 82");
+            $('#correccionpreg1').css({ color: "#bd0000" });
+        } else {
+            if ($('#correccionpreg1').html('') != '') { $('#correccionpreg1').html(''); }
+            $('#correccionpreg1').append("Su respuesta es correcta");
+            $('#correccionpreg1').css({ color: "#10b600" });
+        }
+
+        var campeon2Mundial = [];
+        $('[name=arcampeon]:checked').each(function () {
+            campeon2Mundial.push($(this).val());
+        });
+        if (!campeon2Mundial.includes("vm86")) {
+            if ($('#correccionpreg2').html('') != '') { $('#correccionpreg2').html(''); }
+            $('#correccionpreg2').append("Incorrecto, la respuesta correcta es Mundial 86");
+            $('#correccionpreg2').css({ color: "#bd0000" });
+        } else {
+            if ($('#correccionpreg2').html('') != '') { $('#correccionpreg2').html(''); }
+            $('#correccionpreg2').append("Su respuesta es correcta");
+            $('#correccionpreg2').css({ color: "#10b600" });
+        }
+
+        var masPresencias = [];
+        $('[name=maxpresencias]:checked').each(function () {
+            masPresencias.push($(this).val());
+        });
+        if (!masPresencias.includes("vpjm")) {
+            if ($('#correccionpreg3').html('') != '') { $('#correccionpreg3').html(''); }
+            $('#correccionpreg3').append("Incorrecto, la respuesta correcta es Javier Mascherano");
+            $('#correccionpreg3').css({ color: "#bd0000" });
+        } else {
+            if ($('#correccionpreg3').html('') != '') { $('#correccionpreg3').html(''); }
+            $('#correccionpreg3').append("Su respuesta es correcta");
+            $('#correccionpreg3').css({ color: "#10b600" });
+        }
+
+        var masGoles = [];
+        $('[name=maxgoles]:checked').each(function () {
+            masGoles.push($(this).val());
+        });
+        if (!masGoles.includes("vglm")) {
+            if ($('#correccionpreg4').html('') != '') { $('#correccionpreg4').html(''); }
+            $('#correccionpreg4').append("Incorrecto, la respuesta correcta es Lionel Messi");
+            $('#correccionpreg4').css({ color: "#bd0000" });
+        } else {
+            if ($('#correccionpreg4').html('') != '') { $('#correccionpreg4').html(''); }
+            $('#correccionpreg4').append("Su respuesta es correcta");
+            $('#correccionpreg4').css({ color: "#10b600" });
+        }
+
+
+        var campeones86 = [];
+        $('[name=campeones86]:checked').each(function () {
+            campeones86.push($(this).val());
+        });
+
+        // if ((campeones86.includes('vccc') || campeones86.includes('vcmk')) || (!campeones86.includes('vcor') || !campeones86.includes('vcjb'))) {
+        if ((!campeones86.includes('vccc') && !campeones86.includes('vcmk')) && (campeones86.includes('vcor') && campeones86.includes('vcjb'))) {
+            if ($('#correccionpreg5').html('') != '') { $('#correccionpreg5').html(''); }
+            $('#correccionpreg5').append("Su respuesta es correcta");
+            $('#correccionpreg5').css({ color: "#10b600" });
+        } else {
+            if ($('#correccionpreg5').html('') != '') { $('#correccionpreg5').html(''); }
+            $('#correccionpreg5').append("Incorrecto, la respuesta correcta es Jorge Burruchaga y Oscar Ruggeri");
+            $('#correccionpreg5').css({ color: "#bd0000" });
+        }
+
+        var dtSeleccion = [];
+        $('[name=dtargentina]:checked').each(function () {
+            dtSeleccion.push($(this).val());
+        });
+        // if ((dtSeleccion.includes('vdtcb') || dtSeleccion.includes('vdtcl')) || (!dtSeleccion.includes('vdtab') || !dtSeleccion.includes('vdtas'))) {
+        if ((!dtSeleccion.includes('vdtcb') && !dtSeleccion.includes('vdtcl')) && (dtSeleccion.includes('vdtab') && dtSeleccion.includes('vdtas'))) {
+            if ($('#correccionpreg6').html('') != '') { $('#correccionpreg6').html(''); }
+            $('#correccionpreg6').append("Su respuesta es correcta");
+            $('#correccionpreg6').css({ color: "#10b600" });
+        } else {
+            if ($('#correccionpreg6').html('') != '') { $('#correccionpreg6').html(''); }
+            $('#correccionpreg6').append("Incorrecto, la respuesta correcta es Alfio Basile y Alejandro Sabella");
+            $('#correccionpreg6').css({ color: "#bd0000" });
+        }
+
+        var golMundial2014 = [];
+        $('[name=goles2014]:checked').each(function () {
+            golMundial2014.push($(this).val());
+        });
+        if (!golMundial2014.includes('vgolrp') && (golMundial2014.includes('vgolgh') && golMundial2014.includes('vgolmr') && golMundial2014.includes('vgoldm'))) {
+            if ($('#correccionpreg7').html('') != '') { $('#correccionpreg7').html(''); }
+            $('#correccionpreg7').append("Su respuesta es correcta");
+            $('#correccionpreg7').css({ color: "#10b600" });
+        } else {
+            if ($('#correccionpreg7').html('') != '') { $('#correccionpreg7').html(''); }
+            $('#correccionpreg7').append("Incorrecto, la respuesta correcta es Marcos Rojo, Angel Di Maria y Gonzalo Higuain");
+            $('#correccionpreg7').css({ color: "#bd0000" });
+        }
+
+        var grupoMundial2018 = [];
+        $('[name=rivalesarg]:checked').each(function () {
+            grupoMundial2018.push($(this).val());
+        });
+        if ((!grupoMundial2018.includes('vgrupome') && !grupoMundial2018.includes('vgrupoes') && !grupoMundial2018.includes('vgruporu')) &&
+            (grupoMundial2018.includes('vgrupocr') && grupoMundial2018.includes('vgrupois') && grupoMundial2018.includes('vgruponi'))) {
+            if ($('#correccionpreg8').html('') != '') { $('#correccionpreg8').html(''); }
+            $('#correccionpreg8').append("Su respuesta es correcta");
+            $('#correccionpreg8').css({ color: "#10b600" });
+        } else {
+            if ($('#correccionpreg8').html('') != '') { $('#correccionpreg8').html(''); }
+            $('#correccionpreg8').append("Incorrecto, la respuesta correcta es Nigeria, Islandia y Croacia");
+            $('#correccionpreg8').css({ color: "#bd0000" });
+        }
+
+        if ($('#correccionpreg9').html('') != '') { $('#correccionpreg9').html(''); }
+        $('#correccionpreg9').append("Muchas gracias por tu opinion!");
+        $('#correccionpreg9').css({ color: "#10b600" });
+
+        if ($('#correccionpreg10').html('') != '') { $('#correccionpreg10').html(''); }
+        $('#correccionpreg10').append("Muchas gracias por tu opinion!");
+        $('#correccionpreg10').css({ color: "#10b600" });        
     }
 }
-
-
